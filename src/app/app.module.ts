ddabd07f69850c27970b685d790ca4fb3d165c7b
kamaleshKDK <kamaleshkdk@gmail.com>
2022-09-03 14:11:23 +0530

    angular basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { StrInterpolationComponent } from './str-interpolation/str-interpolation.component';
+import { StrResetComponent } from './str-reset/str-reset.component';
+import { ShowListComponent } from './show-list/show-list.component';
+import { ShowListService } from './show-list/list-service.service';
+import { DefaultPipesComponent } from './default-pipes/default-pipes.component';
+import { CustomPipesComponent } from './custom-pipes/custom-pipes.component';
+import { NewCustomPipe } from './custom-pipes/new-custom.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    StrInterpolationComponent,
+    StrResetComponent,
+    ShowListComponent,
+    DefaultPipesComponent,
+    CustomPipesComponent,
+    NewCustomPipe,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
-  providers: [],
+  providers: [ShowListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
